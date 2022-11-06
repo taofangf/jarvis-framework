6fe92e27fb8413f821d082772a38595989ef8e0a
taofangf <949205444@qq.com>
2022-11-06 21:45:01 +0800

    Polish

--- jarvis-common-web/src/main/java/org/jarvisframework/web/common/configuration/WebCommonConfiguration.java
+++ jarvis-common-web/src/main/java/org/jarvisframework/web/common/configuration/WebCommonConfiguration.java
@@ -17,7 +17,6 @@
 
 package org.jarvisframework.web.common.configuration;
 
-import com.alibaba.fastjson.serializer.SerializeFilter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
@@ -33,7 +32,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
